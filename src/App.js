75734c5ccddb073c8c3480376be13af9bb581713
Vineeth Rai K <vineethraik23082000@gmail.com>
2024-01-20 16:09:59 +0530

    Add building state to App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BuildingState from "./common/BuildingState/BuildingState";
+import { buildingStateConstants } from "./constants/stringConstants";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BuildingState
+        textData={buildingStateConstants.TEXTS.CODE}
+        loadingText={buildingStateConstants.LOADING_TEXT.CODE}
+      />
+    </>
   );
 }
 
