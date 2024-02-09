e41388b410a5eea025fbcc73ba4158fb0ddbc5b0
Vineeth Rai K <vineethraik23082000@gmail.com>
2024-02-09 20:43:29 +0530

    Add buildingState for the Contact Us page

--- src/container/ContactUs/ContactUs.js
+++ src/container/ContactUs/ContactUs.js
@@ -1,12 +1,16 @@
+import BuildingState from "common/BuildingState/BuildingState";
 import PageContainer from "container/PageContainer/PageContainer";
 import React from "react";
 
 export function ContactUs() {
   return (
     <PageContainer
-      containerClassName={"d-flex justify-content-center align-items-center"}
+      containerClassName={
+        "d-flex flex-column justify-content-center align-items-center"
+      }
     >
       ContactUs
+      <BuildingState />
     </PageContainer>
   );
 }
