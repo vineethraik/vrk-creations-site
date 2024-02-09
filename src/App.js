0387548144c0bb18b0bed17701e05ee1f5c9f664
Vineeth Rai K <vineethraik23082000@gmail.com>
2024-02-09 20:37:16 +0530

    Add theme constant class for the app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Router from "container/Router/Router";
 import "./App.css";
 
 function App() {
-  return <Router />;
+  return <Router className={"theme__light"} />;
 }
 
 export default App;
